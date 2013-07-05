78fdd7728c3d6ca506c78d9c5ff1fc9dc4172f91
Aligorith <aligorith@gmail.com>
2013-07-06 01:48:54 +1200

    Start of very rough Depsgraph Public API

--- BKE_depsgraph.h
+++ BKE_depsgraph.h
@@ -29,6 +29,70 @@
 #ifndef __BKE_DEPSGRAPH_H__
 #define __BKE_DEPSGRAPH_H__
 
+/* Dependency Graph
+ *
+ * The dependency graph tracks relations between various pieces of data in
+ * a Blender file, but mainly just those which make up scene data. It is used
+ * to determine the set of operations need to ensure that all data has been
+ * correctly evaluated in response to changes, based on dependencies and visibility
+ * of affected data.
+ *
+ *
+ * Evaluation Engine
+ *
+ * The evaluation takes the operation-nodes the Depsgraph has tagged for updating, 
+ * and schedules them up for being evaluated/executed such that the all dependency
+ * relationship constraints are satisfied. 
+ */
 
+/* ************************************************* */
+/* Forward-defined typedefs for core types
+ * - These are used in all depsgraph code and by all callers of Depsgraph API...
+ */
+
+/* Dependency Graph */
+typedef struct Depsgraph Depsgraph;
+
+/* Base "Node"-Type in Depsgraph */
+typedef struct DepsNode DepsNode;
+
+/* ************************************************ */
+/* Depsgraph API */
+
+/* CRUD ------------------------------------------- */
+
+// Get main depsgraph instance from context!
+
+/* Create new Depsgraph instance */
+// TODO: what args are needed here? What's the building-graph entrypoint?
+Depsgraph *DEG_graph_new(void);
+
+/* Free Depsgraph and all its data */
+void DEG_free(Depsgraph *graph);
+
+/* Update Tagging -------------------------------- */
+
+/* Tag node(s) associated with states such as time and visibility */
+void DEG_scene_update_flags(Depsgraph *graph, const bool do_time);
+void DEG_on_visible_update(Depsgraph *graph, const bool do_time);
+
+/* Tag node(s) associated with changed data for later updates */
+void DEG_id_tag_update(Depsgraph *graph, const ID *id);
+void DEG_data_tag_update(Depsgraph *graph, const PointerRNA *ptr);
+void DEG_property_tag_update(Depsgraph *graph, const PointerRNA *ptr, const PropertyRNA *prop);
+
+/* Flush updates */
+void DEG_scene_flush_update(Depsgraph *graph);
+
+/* ************************************************ */
+/* Evaluation Engine API */
+
+/* Frame changed recalculation entrypoint */
+void DEG_evaluate_on_framechange(Depsgraph *graph, double ctime);
+
+/* Data changed recalculation entrypoint */
+void DEG_evaluate_on_refresh(Depsgraph *graph);
+
+/* ************************************************ */
 
 #endif // __BKE_DEPSGRAPH_H__
